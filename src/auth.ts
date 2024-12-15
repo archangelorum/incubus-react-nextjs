e4d46e310d6ff1975f4a181ea00fd56e7216aeb3
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-12-15 17:51:16 +0100

    Github auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "../prisma/prisma"
 import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -16,6 +17,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           image: profile.picture,
           roleId: 1
         }
+      }
+    }),
+    GitHub({
+      profile(profile){
+        const roleId = profile.login === "archangelorum" ? 2 : 1; //isAdmin
+
+        return {
+          id: profile.id.toString(),
+          name: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          roleId: roleId
+        };
       }
     })
   ],
