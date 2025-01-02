fa50eb79bd37e21bcd95362834dd925125379b5a
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2025-01-02 14:42:07 +0100

    Refactor: clean up

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,9 +2,11 @@ import Navbar from "@/components/Navbar";
 
 export default function Layout({children}: Readonly<{children: React.ReactNode}>) {
     return (
-        <main className="font-work-sans">
+        <>
             <Navbar />
-            {children}
-        </main>
+            <main>
+                {children}
+            </main>
+        </>
     )
 }
